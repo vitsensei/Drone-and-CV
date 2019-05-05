e3baeeb6e8c3a040d2060c16eea6047b8a03bfe3
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-05-05 22:25:31 +0930

    Update drone_orientation.py

--- Host Computer/drone_orientation.py
+++ Host Computer/drone_orientation.py
@@ -1,18 +1,28 @@
 import socket
 import time
+import cv2
+import numpy as np
+from copy import deepcopy
+from math import cos, sin
 
 def bytes2coor(byte_fnc):
 	receivedCoor_fnc = [0, 0, 0]
 
-	receivedCoor_fnc[0] = ((-1)**(byte_fnc[0]>>7)) * ((byte_fnc[1]) | (((byte_fnc[0]&0x7f)<<8)))
-	receivedCoor_fnc[1] = ((-1)**(byte_fnc[2]>>7)) * ((byte_fnc[3]) | (((byte_fnc[2]&0x7f)<<8)))
-	receivedCoor_fnc[2] = ((-1)**(byte_fnc[4]>>7)) * ((byte_fnc[5]) | (((byte_fnc[4]&0x7f)<<8)))
+	receivedCoor_fnc[0] = float(((-1)**(byte_fnc[0]>>7)) * ((byte_fnc[1]) | (((byte_fnc[0]&0x7f)<<8))))
+	receivedCoor_fnc[1] = float(((-1)**(byte_fnc[2]>>7)) * ((byte_fnc[3]) | (((byte_fnc[2]&0x7f)<<8))))
+	receivedCoor_fnc[2] = float(((-1)**(byte_fnc[4]>>7)) * ((byte_fnc[5]) | (((byte_fnc[4]&0x7f)<<8))))
 
 	return receivedCoor_fnc
 
 
 if __name__ == '__main__':
 	# Initial setup
+
+	# create a blank canvas
+	img = np.zeros((512, 512, 3), np.uint8)
+	img.fill(255)
+
+	# Setup server and client
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	s.bind((socket.gethostname(), 1234)) # bind(ip, port)
@@ -69,7 +79,15 @@ if __name__ == '__main__':
 
 							receivedAngle = bytes2coor(full_msg)
 							print(f"coordinate received: {receivedAngle}")
-						
+
+							draw_point = (256 + int(100*cos(receivedAngle[2]/180.0*3.1415)),256 - int(100*sin(receivedAngle[2]/180.0*3.1415)))
+							print(f"Draw point: {draw_point}")
+
+							img.fill(255)
+							cv2.arrowedLine(img, (256, 256), (draw_point[0], draw_point[1]), (0, 0, 0), thickness = 5)
+							cv2.imshow("Drone orientation",img)
+							cv2.waitKey(1)
+
 						except socket.timeout:
 							print("Time out. Will try again.")
 
